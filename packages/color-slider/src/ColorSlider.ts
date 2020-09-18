0ba947d1222f6572138e4e9e4e091644e91273ac
Westbrook Johnson <westbrook.johnson@gmail.com>
2020-09-18 13:51:47 -0400

    docs: build them with 11ty, wip

--- packages/color-slider/src/ColorSlider.ts
+++ packages/color-slider/src/ColorSlider.ts
@@ -364,6 +364,12 @@ export class ColorSlider extends Focusable {
         this.handlePointermove(event);
     }
 
+    private get handlePositionStyles(): string {
+        return `${this.vertical ? 'top' : 'left'}: ${
+            this.sliderHandlePosition
+        }%`;
+    }
+
     protected render(): TemplateResult {
         return html`
             <div
@@ -386,8 +392,7 @@ export class ColorSlider extends Focusable {
                 class="handle"
                 color="hsl(${this.value}, 100%, 50%)"
                 ?disabled=${this.disabled}
-                style="${this.vertical ? 'top' : 'left'}: ${this
-                    .sliderHandlePosition}%"
+                style=${this.handlePositionStyles}
                 @manage=${streamingListener(
                     { type: 'pointerdown', fn: this.handlePointerdown },
                     { type: 'pointermove', fn: this.handlePointermove },
